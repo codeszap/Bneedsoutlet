fd076d7d36c0ec6432f01929a27f1a0108481b41
codeszap <sulthanmydeen191997@gmail.com>
2023-10-05 19:06:39 +0530

    Add SalesReport Screen

---
+++ lib/screens/sales_report.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class sales_report extends StatefulWidget {
+  const sales_report({super.key});
+
+  @override
+  State<sales_report> createState() => _sales_reportState();
+}
+
+class _sales_reportState extends State<sales_report> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
